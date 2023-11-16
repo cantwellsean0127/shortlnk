f962b74deb252a93f61ac0910a9fa1b35a13d01b
Sean Cantwell <cantwellsean0127@gmail.com>
2023-11-16 12:40:01 -0600

    Added API create functionality.

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,21 @@ import database_client from "./database.js"
 // Configures our enviroment variables
 dotenv.config()
 
-// TO-DO: Create routes here
+// Whenever this route is called, create a new URL
+const createShortenedURL = async (req, res) => {
+
+    // Generates the shortened URL
+    let database_response = await database_client.query("SELECT TO_HEX(nextval('shortened_url_sequence'));")
+    const shortended_url = `${process.env.server_address}/${database_response.rows[0].to_hex}`
+
+    // Performs the query and returns the results to the client
+    const query_options = [req.body.name, req.body.target_url, shortended_url]
+    database_response = await database_client.query("INSERT INTO urls (name, target_url, shortened_url) VALUES ($1, $2, $3) RETURNING *;", query_options)
+    res.json(database_response.rows[0])
+}
+
 
 // Exports our routes
-export default {}
+export default {
+    createShortenedURL: createShortenedURL
+}
--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ import routes from "./routes.js"
 // Creates our express server instance
 const server = express()
 
-// TO-DO: Insert routes here
+// This built-in middleware parses the body of any incoming requests and converts it into an object if the body is a JSON string
+server.use(express.json())
+
+// Route for creating a new url
+server.post("/api/urls", routes.createShortenedURL)
 
 // Exports our server object
 export default server
