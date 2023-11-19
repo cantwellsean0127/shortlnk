9c853dffd64b80652937bb83a93dda6c2bdfdc51
Sean Cantwell <cantwellsean0127@gmail.com>
2023-11-19 10:01:07 -0600

    Solved issue #15

--- src/server-settings.js
+++ src/server-settings.js
@@ -1,4 +1,4 @@
 // When this module is imported and invoked, set the server settings
 export default (server) => {
-    server.set("trust proxy", true)
+    server.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
 }
