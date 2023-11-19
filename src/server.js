9bddb39ef6530f17974668ee7e344ad5e6c77807
Sean Cantwell <cantwellsean0127@gmail.com>
2023-11-18 21:18:50 -0600

    Limits clients to 100 requests per 15 minutes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ import express from "express"
 // The cookie-parser module allows us to use and manage cookies
 import cookie_parser from "cookie-parser"
 
+// The express-rate-limit module allows us to limit the rate at which requests are handled
+import express_rate_limit from "express-rate-limit"
+
 // Imports our server routes
 import routes from "./routes.js"
 
@@ -12,6 +15,9 @@ const server = express()
 
 // This built-in middleware parses the body of any incoming requests and converts it into an object if the body is a JSON string
 server.use(express.json())
+
+// This build-in middleware rate limits requests to 100 per 15 minutes
+server.use(express_rate_limit({ windowMs: 15 * 60 * 1000, max: 100 }))
 
 // Middleware that will statically server files within the public directory. This is where our front-end file will be located.
 server.use(express.static("./public"))
