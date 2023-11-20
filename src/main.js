c66f4010793270162780866ac5ae838c04b6d644
Sean Cantwell <103895364+cantwellsean0127@users.noreply.github.com>
2023-11-20 12:38:04 -0600

    Solved issue #26

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
+// The dotenv module allows us to use enviroment variables stored in the .env file
+import dotenv from "dotenv"
+
 // Imports our server object
 import server from "./server.js"
 
 // This is where program execution begins
 const start = () => {
 
-    // Listens for incoming connections on port 80
-    server.listen(80)
+    // Configures our enviroment variables
+    dotenv.config()
+
+    // Listens for incoming connections
+    server.listen(process.env.server_port)
 }
 
 // Starts our program
